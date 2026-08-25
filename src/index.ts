9c786433f2cc6ba21f8a70ac46d1f00ad42e263b
Pabil Adhikari <your-github-email@example.com>
2026-08-24 22:47:09 -0700

    part 1 express server setup

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// Init our express server
+import express from "express";
+const app = express();
+const port = "3000";
+
+app.get("/", (req, res) => {
+    res.send("hellow World");
+    console.log("Response sent")
+
+})
+app.get("/health", (req, res) => {
+    res.send("hellow World");
+    console.log("Response sent")
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+
+});
