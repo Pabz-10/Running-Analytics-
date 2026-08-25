2b42f92cacd06f11988734e551bf9415f6d13967
Pabil Adhikari <your-github-email@example.com>
2026-08-24 23:37:50 -0700

    to sleepy, finish tomorrow

---
+++ src/db.ts
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+import knex from "knex";
+
+dotenv.config();
+
+import knex from "knex";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+export default db;
